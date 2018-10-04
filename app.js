7442614215441b0e785bf3b1c569570374635b42
b-sonter <40931305+b-sonter@users.noreply.github.com>
2018-10-05 08:27:03 +1000

    Completed task 1 and 2
    created new git repo, installed mongodb driver, created app.js file and added code to connect to the server and database mydb

---
+++ app.js
@@ -0,0 +1,19 @@
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/mydb";
+
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.json());
+
+//connect to server
+console.log("Setting up initial db state... ");
+MongoClient.connect(url, function(err, db){
+  if(err) throw err;
+
+  my_database = "mydb"
+  let dbo = db.db(my_database);
+  console.log("Succesfully connected to " + my_database);
+  db.close();
+});
